c4ec558e7f01c782b675fd6ddcfd9eb2cf60a363
YJuDeAd <robinrashi@gmail.com>
2026-09-08 17:46:18 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from "./components/navbar.tsx";
+
 import Home from './pages/Home.tsx';
 import Projects from './pages/Projects.tsx';
 import Contact from './pages/Contact.tsx';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
